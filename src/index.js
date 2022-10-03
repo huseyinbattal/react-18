cc0f4156158bc93e8927947297b336768f8b7b2f
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-10-03 22:59:30 +0300

    flushSync

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import * as ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root"),
-  () => console.log("app starting...")
-);
+const root = document.getElementById("root");
+const renderer = ReactDOM.createRoot(root);
+
+renderer.render(<App />);
