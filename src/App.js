b80826fd56773545d9ddc0b2a8f3ae8d8e0c2d85
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-10-05 20:56:00 +0300

    prettier

--- src/App.js
+++ src/App.js
@@ -2,19 +2,17 @@ import "./App.css";
 import { useSyncExternalStore } from "react";
 
 function App() {
-  const screenWidth = useSyncExternalStore((listener) => {
-    window.addEventListener("resize", listener)
-    
-    return () => {
-  window.removeEventListener("resize",listener)
-}
+  const screenWidth = useSyncExternalStore(
+    (listener) => {
+      window.addEventListener("resize", listener);
 
-  },()=>window.innerWidth)
-  return (
-    <>
-    {screenWidth}
-    </>
- )
+      return () => {
+        window.removeEventListener("resize", listener);
+      };
+    },
+    () => window.innerWidth
+  );
+  return <>{screenWidth}</>;
 }
 
 export default App;
